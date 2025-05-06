56a9cf14222ad6275e8ebd03e9d238e10ea967c4
dhani.frans <dhani.francisco.9@gmail.com>
2025-05-07 03:06:41 +0700

    [BAB 6] ANIMATION

--- src/components/ItemSmall.jsx
+++ src/components/ItemSmall.jsx
@@ -1,96 +1,157 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Animated, Dimensions } from 'react-native';
 import FastImage from '@d11/react-native-fast-image';
-import { ArrowRight3, Clock, Eye} from 'iconsax-react-native';
+import { ArrowRight3, Clock, Eye } from 'iconsax-react-native';
 import { fontType, colors } from '../theme';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
-const ItemSmall = ({item}) => {
-    return (
-      <TouchableOpacity style={styles.cardItem}>
-        <FastImage
-          style={styles.cardImage}
-          source={{
-            uri: item.image,
-            headers: {Authorization: 'someAuthToken'},
-            priority: FastImage.priority.high,
-          }}
-          resizeMode={FastImage.resizeMode.cover}
-        />
-        <View style={styles.cardContent}>
-          <View
-            style={{
-              flexDirection: 'row',
-              gap:30
-            }}>
-            <View style={{gap: 5, flex:1}}>
-              <Text style={styles.cardCategory}>{item.category}</Text>
-              <Text style={styles.cardTitle}>{item.title}</Text>
-            </View>
-            <ArrowRight3
-              color={"#FF8A65"}
-              variant="Linear"
-              size={20}
-            />
-          </View>
-          <View style={styles.cardInfo}>
-            <Clock size={10} variant="Linear" color={"#FF8A65"} />
-            <Text style={styles.cardText}>{item.date}</Text>
-            <Eye
-              size={10}
-              variant="Linear"
-              color={"#FF8A65"}
-            />
-            <Text style={styles.cardText}>{item.views}</Text>
-          </View>
-        </View>
-      </TouchableOpacity>
+const ItemSmall = ({ item }) => {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const loadAnim = new Animated.Value(0);
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    const loadLoop = Animated.loop(
+      Animated.timing(loadAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      })
     );
-  };
-  export default ItemSmall;
-  const styles = StyleSheet.create({
-    listCard: {
-      paddingHorizontal: 24,
-      paddingVertical: 10,
-      gap: 5,
-    },
-    cardItem: {
-      backgroundColor: colors.blue(0.03),
-      flexDirection: 'row',
-      borderRadius: 10,
-    },
-    cardCategory: {
-      color: "#E43C38",
-      fontSize: 10,
-      fontFamily: fontType['Pjs-SemiBold'],
-    },
-    cardTitle: {
-      fontSize: 14,
-      fontFamily: fontType['Pjs-Bold'],
-      color: colors.white(),
-    },
-    cardText: {
-      fontSize: 10,
-      fontFamily: fontType['Pjs-Medium'],
-      color: colors.white(0.9)
-    },
-    cardImage: {
-      width: 94,
-      height: 94,
-      borderRadius: 10,
-      resizeMode: 'cover',
-    },
-    cardInfo: {
-      flexDirection: 'row',
-      gap: 5,
-      alignItems: 'center',
-    },
-    cardContent: {
-      gap: 10,
-      justifyContent: 'space-between',
-      paddingRight: 10,
-      paddingLeft: 15,
-      flex: 1,
-      paddingVertical: 10,
-    },
+
+    loadLoop.start();
+
+    return () => {
+      clearTimeout(timer);
+      loadLoop.stop();
+    };
   });
+
+  const loadTranslate = loadAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-200, Dimensions.get('window').width],
+  });
+
+  return (
+    isLoading ? <View style={loader.card}>
+      <Animated.View style={[
+        loader.strip,
+        {
+          transform: [
+            { translateX: loadTranslate },
+            { rotate: '20deg' },
+          ],
+        },
+      ]} />
+    </View> : <TouchableOpacity style={styles.cardItem}>
+      <FastImage
+        style={styles.cardImage}
+        source={{
+          uri: item.image,
+          headers: { Authorization: 'someAuthToken' },
+          priority: FastImage.priority.high,
+        }}
+        resizeMode={FastImage.resizeMode.cover}
+      />
+      <View style={styles.cardContent}>
+        <View
+          style={{
+            flexDirection: 'row',
+            gap: 30
+          }}>
+          <View style={{ gap: 5, flex: 1 }}>
+            <Text style={styles.cardCategory}>{item.category}</Text>
+            <Text style={styles.cardTitle}>{item.title}</Text>
+          </View>
+          <ArrowRight3
+            color={"#FF8A65"}
+            variant="Linear"
+            size={20}
+          />
+        </View>
+        <View style={styles.cardInfo}>
+          <Clock size={10} variant="Linear" color={"#FF8A65"} />
+          <Text style={styles.cardText}>{item.date}</Text>
+          <Eye
+            size={10}
+            variant="Linear"
+            color={"#FF8A65"}
+          />
+          <Text style={styles.cardText}>{item.views}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+export default ItemSmall;
+const styles = StyleSheet.create({
+  listCard: {
+    paddingHorizontal: 24,
+    paddingVertical: 10,
+    gap: 5,
+  },
+  cardItem: {
+    backgroundColor: colors.blue(0.03),
+    flexDirection: 'row',
+    borderRadius: 10,
+  },
+  cardCategory: {
+    color: "#E43C38",
+    fontSize: 10,
+    fontFamily: fontType['Pjs-SemiBold'],
+  },
+  cardTitle: {
+    fontSize: 14,
+    fontFamily: fontType['Pjs-Bold'],
+    color: colors.white(),
+  },
+  cardText: {
+    fontSize: 10,
+    fontFamily: fontType['Pjs-Medium'],
+    color: colors.white(0.9)
+  },
+  cardImage: {
+    width: 94,
+    height: 94,
+    borderRadius: 10,
+    resizeMode: 'cover',
+  },
+  cardInfo: {
+    flexDirection: 'row',
+    gap: 5,
+    alignItems: 'center',
+  },
+  cardContent: {
+    gap: 10,
+    justifyContent: 'space-between',
+    paddingRight: 10,
+    paddingLeft: 15,
+    flex: 1,
+    paddingVertical: 10,
+  },
+});
+
+const loader = StyleSheet.create({
+  card: {
+    width: Dimensions.get('window').width * 0.88,
+    height: 94,
+    marginRight: 10,
+    overflow: 'hidden',
+    position: 'relative',
+    backgroundColor: '#e1e1e1',
+    borderRadius: 10,
+  },
+  strip: {
+    position: 'absolute',
+    width: '30%',
+    height: '300%',
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    top: '-100%',
+    left: 0,
+  },
+});
--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -1,10 +1,10 @@
-import { ScrollView, StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 import { Setting, Edit } from 'iconsax-react-native';
 import React from 'react';
 import FastImage from '@d11/react-native-fast-image';
-import { ProfileData,  PictureList } from '../../data';
+import { ProfileData, PictureList } from '../../data';
 import { fontType, colors } from '../../theme';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
 const formatNumber = number => {
   if (number >= 1_000_000_000) return (number / 1_000_000_000).toFixed(1).replace(/\.0$/, '') + 'B';
@@ -13,115 +13,115 @@ const formatNumber = number => {
   return number.toString();
 };
 
+const profileInfo = [
+  { label: 'Posted', value: ProfileData.blogPosted },
+  { label: 'Following', value: formatNumber(ProfileData.following) },
+  { label: 'Follower', value: formatNumber(ProfileData.follower) }
+];
 
 const Profile = () => {
   const navigation = useNavigation();
-    return (
-      <View style={styles.container}>
-        {/* Header */}
-        <View style={styles.header}>
-          <Setting color={colors.orange()} variant="Linear" size={24} />
-        </View>
-  
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          {/* Profile Section */}
-          <View style={styles.profileSection}>
+
+  const renderHeader = () => (
+    <>
+      {/* Header */}
+      <View style={styles.header}>
+        <Setting color={colors.orange()} variant="Linear" size={24} />
+      </View>
+
+      {/* Profile Section */}
+      <View style={styles.profileSection}>
+        <FastImage
+          style={styles.pic}
+          source={{
+            uri: ProfileData.profilePict,
+            headers: { Authorization: 'someAuthToken' },
+            priority: FastImage.priority.high,
+          }}
+          resizeMode={FastImage.resizeMode.cover}
+        />
+        <Text style={styles.name}>{ProfileData.name}</Text>
+        <Text style={styles.info}>Member since {ProfileData.createdAt}</Text>
+      </View>
+
+      {/* Stats Section */}
+      <View style={styles.statsContainer}>
+        {profileInfo.map((stat, index) => (
+          <View key={index} style={styles.statItem}>
+            <Text style={styles.statCount}>{stat.value}</Text>
+            <Text style={styles.statLabel}>{stat.label}</Text>
+          </View>
+        ))}
+      </View>
+
+      {/* Edit Profile Button */}
+      <TouchableOpacity style={styles.buttonEdit}>
+        <Text style={styles.buttonText}>Edit Profile</Text>
+      </TouchableOpacity>
+    </>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={PictureList.slice(0, 12)}
+        keyExtractor={(_, index) => index.toString()}
+        numColumns={3}
+        ListHeaderComponent={renderHeader}
+        columnWrapperStyle={{ justifyContent: 'space-between' }}
+        renderItem={({ item }) => (
+          <View style={styles.imageContainer}>
             <FastImage
-              style={styles.pic}
-              source={{
-                uri: ProfileData.profilePict,
-                headers: { Authorization: 'someAuthToken' },
-                priority: FastImage.priority.high,
-              }}
+              source={{ uri: item.image }}
+              style={styles.image}
               resizeMode={FastImage.resizeMode.cover}
             />
-            <Text style={styles.name}>{ProfileData.name}</Text>
-            <Text style={styles.info}>Member since {ProfileData.createdAt}</Text>
           </View>
-  
-          {/* Stats Section */}
-          <View style={styles.statsContainer}>
-            {[
-              { label: 'Posted', value: ProfileData.blogPosted },
-              { label: 'Following', value: formatNumber(ProfileData.following) },
-              { label: 'Follower', value: formatNumber(ProfileData.follower) }
-            ].map((stat, index) => (
-              <View key={index} style={styles.statItem}>
-                <Text style={styles.statCount}>{stat.value}</Text>
-                <Text style={styles.statLabel}>{stat.label}</Text>
-              </View>
-            ))}
-          </View>
-  
-          {/* Edit Profile Button */}
-          <TouchableOpacity style={styles.buttonEdit}>
-            <Text style={styles.buttonText}>Edit Profile</Text>
-          </TouchableOpacity>
-  
-          {/* Blog Posts Grid */}
-          <FlatList
-            data={PictureList.slice(0, 12)}
-            keyExtractor={(_, index) => index.toString()}
-            numColumns={3}
-            columnWrapperStyle={{ justifyContent: 'space-between' }}
-            renderItem={({ item }) => (
-              <View style={styles.imageContainer}>
-                <FastImage
-                  source={{ uri: item.image }}
-                  style={styles.image}
-                  resizeMode={FastImage.resizeMode.cover}
-                />
-              </View>
-            )}
-            contentContainerStyle={styles.blogSection}
-          />
-        </ScrollView>
-        <TouchableOpacity
+        )}
+        contentContainerStyle={styles.scrollContainer}
+      />
+
+      <TouchableOpacity
         style={styles.floatingButton}
         onPress={() => navigation.navigate('AddBlog')}>
         <Edit color={colors.white()} variant="Linear" size={20} />
       </TouchableOpacity>
-        
-      </View>
-    );
-  };
-  
-  export default Profile;
-  
-  const styles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: colors.black() },
-    header: { padding: 16, alignItems: 'flex-end' },
-    scrollContainer: { paddingHorizontal: 24, paddingVertical: 20 },
-    profileSection: { alignItems: 'center', marginBottom: 20 },
-    pic: { width: 150, height: 150, borderRadius: 100 },
-    name: { color: colors.white(), fontSize: 25, fontFamily: fontType['Pjs-Bold'] },
-    info: { fontSize: 12, color: colors.white(0.7), fontFamily: fontType['Pjs-Regular'] },
-    statsContainer: { flexDirection: 'row', justifyContent: 'space-around', marginBottom: 15 },
-    statItem: { alignItems: 'center' },
-    statCount: { fontSize: 18, color: colors.white(), fontFamily: fontType['Pjs-SemiBold'] },
-    statLabel: { fontSize: 14, color: colors.white(0.7), fontFamily: fontType['Pjs-Regular'] },
-    buttonEdit: { padding: 14, backgroundColor: colors.orange(), borderRadius: 10, alignItems: 'center' },
-    buttonText: { fontSize: 14, color: colors.black(), fontFamily: fontType['Pjs-SemiBold'] },
-    blogSection: { paddingTop: 15, paddingHorizontal: 5 },
-    imageContainer: { flex: 1, margin: 5 },
-    image: { width: '100%', aspectRatio: 1, borderRadius: 10 },
-    floatingButton: {
-      backgroundColor: colors.orange(),
-      padding: 15,
-      position: 'absolute',
-      bottom: 24,
-      right: 24,
-      borderRadius: 10,
-      shadowColor: colors.blue(),
-      shadowOffset: {
-        width: 0,
-        height: 4,
-      },
-      shadowOpacity: 0.3,
-      shadowRadius: 4.65,
-  
-      elevation: 8,
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: colors.black() },
+  header: { padding: 16, alignItems: 'flex-end' },
+  scrollContainer: { paddingHorizontal: 24, paddingVertical: 20 },
+  profileSection: { alignItems: 'center', marginBottom: 20 },
+  pic: { width: 150, height: 150, borderRadius: 100 },
+  name: { color: colors.white(), fontSize: 25, fontFamily: fontType['Pjs-Bold'] },
+  info: { fontSize: 12, color: colors.white(0.7), fontFamily: fontType['Pjs-Regular'] },
+  statsContainer: { flexDirection: 'row', justifyContent: 'space-around', marginBottom: 15 },
+  statItem: { alignItems: 'center' },
+  statCount: { fontSize: 18, color: colors.white(), fontFamily: fontType['Pjs-SemiBold'] },
+  statLabel: { fontSize: 14, color: colors.white(0.7), fontFamily: fontType['Pjs-Regular'] },
+  buttonEdit: { padding: 14, backgroundColor: colors.orange(), borderRadius: 10, alignItems: 'center', marginBottom: 15 },
+  buttonText: { fontSize: 14, color: colors.black(), fontFamily: fontType['Pjs-SemiBold'] },
+  imageContainer: { flex: 1, margin: 5 },
+  image: { width: '100%', aspectRatio: 1, borderRadius: 10 },
+  floatingButton: {
+    backgroundColor: colors.orange(),
+    padding: 15,
+    position: 'absolute',
+    bottom: 24,
+    right: 24,
+    borderRadius: 10,
+    shadowColor: colors.blue(),
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-  
-  });
-  
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+});
